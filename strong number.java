0f37ed0c70560702ee3f5f62d09fa09954d80653
akshithaveluri <151708100+akshithaveluri@users.noreply.github.com>
2024-08-22 12:10:07 +0530

    Create strong number.java

---
+++ strong number.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+   public static void main(String[] args)
+   {
+      int inputNumber = 143;
+      int i;
+      int factorial,digit;
+      int sum = 0;
+      int temp = inputNumber;
+      while(temp != 0) {
+         i = 1;
+         factorial = 1;
+         digit = temp % 10;
+         while(i <= digit) {
+            factorial = factorial * i;
+            i++;
+         }
+         sum = sum + factorial;
+         temp = temp / 10;
+      }
+      if(sum == inputNumber)
+         System.out.println(inputNumber + " is a strong number\n");
+      else
+         System.out.println(inputNumber + " is not a strong number\n");
+   }   
+}
