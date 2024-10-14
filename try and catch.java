358a2cdc3c4a4c4a4cf0363310181883c2167b62
akshithaveluri <151708100+akshithaveluri@users.noreply.github.com>
2024-10-14 12:16:48 +0530

    Create try and catch.java

---
+++ try and catch.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        int a=5,b=0,c;
+        try{
+            c=a/b;
+            System.out.println("Value of c is:"+c);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Exception caught");
+        }
+    }
+}
